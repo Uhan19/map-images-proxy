2e26ab6cb34c03a3ec3251a39fc8ac34fc2fc4ae
Yuehan Duan <yuehanduan@yahoo.com>
2018-03-26 11:53:41 -0700

    Checkpoint before refactor redis to proxy.

--- proxy/index.js
+++ proxy/index.js
@@ -1,17 +1,21 @@
+require("newrelic");
 const express = require("express");
-const morgan = require("morgan");
 const path = require("path");
 var bodyParser = require("body-parser");
 var router = require("./router.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
 app.use("/:id", express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 
 app.use("/", router);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("broken");
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
--- proxy/router.js
+++ proxy/router.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const request = require("request");
 
 router.get("/map-and-images/business/:id", (req, res) => {
-  request(
-    `http://localhost:3001/map-and-images/business/${req.params.id}`
-  ).pipe(res);
+  request(`http://localhost:3001/map-and-images/business/${req.params.id}`)
+    .on("error", err => {
+      console.log(err);
+    })
+    .pipe(res);
 });
 
 router.get("/map-and-images/business/:id/photos", (req, res) => {
   request(
     `http://localhost:3001/map-and-images/business/${req.params.id}/photos`
-  ).pipe(res);
+  )
+    .on("error", err => {
+      console.log(err);
+    })
+    .pipe(res);
 });
 
 module.exports = router;
