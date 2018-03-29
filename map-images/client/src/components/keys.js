7173fa20863f17bd9ea9303e44c4294db3445466
Yuehan Duan <yuehanduan@yahoo.com>
2018-03-29 14:42:55 -0700

    Delete the mapimages folder.

--- map-images/client/src/components/keys.js
+++
@@ -1,2 +0,0 @@
-var GOOGLE_API_KEY = "AIzaSyAysrl-RVCPXbCIGf2Zf_gankxLtlMmrI8";
-export { GOOGLE_API_KEY };
--- map-images/client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { MapAndImages } from "./components/App.jsx";
-
-ReactDOM.render(<MapAndImages />, document.getElementById("MapAndImages"));
