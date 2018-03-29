dea05f32c57ec62aa59da7faf03200bf0a7d0d5b
Yuehan Duan <yuehanduan@yahoo.com>
2018-03-28 18:40:58 -0700

    Refactor to use deployed microservice ip address.

--- proxy/router.js
+++ proxy/router.js
@@ -63,7 +63,7 @@ router.get("/:id", function(req, res) {
 
 router.get("/map-and-images/business/:id", cacheBusiness, function(req, res) {
   axios
-    .get(`http://localhost:3001/map-and-images/business/${req.params.id}`) //before deployment change this to 54.183.5.244:3001
+    .get(`http://54.183.5.244:3001/map-and-images/business/${req.params.id}`) //before deployment change this to 54.183.5.244:3001
     .then(results => {
       client.setex(req.params.id, 3600, JSON.stringify(results.data));
       res.send(results.data);
@@ -81,7 +81,7 @@ router.get("/map-and-images/business/:id/photos", cachePhoto, function(
 ) {
   axios
     .get(
-      `http://localhost:3001/map-and-images/business/${req.params.id}/photos`
+      `http://54.183.5.244:3001/map-and-images/business/${req.params.id}/photos`
     )
     .then(results => {
       var key = JSON.stringify(req.params.id + 1);
